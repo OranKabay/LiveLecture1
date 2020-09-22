bc3bdb8864a04358c194e90f9fa53f61b6d7c31a
Oran <oran@emetrix.biz>
2020-09-22 15:45:28 +0100

    first commit

---
+++ src/BurgerPayment.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BurgerPayment {
+    public static void main(String[] args) throws Exception {
+        System.out.println("How many burgers would you like?");
+        
+        double value = 101.0/4.0; //if double remember decimal places for full answer
+        System.out.println(value);
+
+        Scanner reader = new Scanner(System.in);
+        int noOfBurgers = reader.nextInt();
+        
+        System.out.println("How many fries would you like?");
+        int noOfChips = reader.nextInt();
+
+        double priceOfBurgers = 4.5;  //In British Pounds
+        double priceOfChips = 1.6;
+        
+        int calOfBurger = 2000;
+        int calOfChips = 500;
+
+        System.out.println("The number of calories of burgers is " + calOfBurger*noOfBurgers + ".\n");
+        System.out.println("The price of the burgers is " + priceOfBurgers*noOfBurgers + ".\n");
+        System.out.println("The number of calories of chips is " + calOfChips*noOfChips + ".\n");
+        System.out.println("The price of the chips is " + priceOfChips*noOfChips + ".\n");
+    }
+}
